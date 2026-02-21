c3099ac42546afefa0888e3ba4d8ef7197d12ba9
kp1153 <kamta_tiwari@yahoo.com>
2026-02-21 15:12:21 +0530

    proxy auth added

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 
 export function proxy(req) {
   const auth = req.cookies.get("auth")?.value
-  if (auth !== process.env.ADMIN_SECRET) {
+  if (!auth || auth !== process.env.ADMIN_SECRET) {
     return NextResponse.redirect(new URL("/login", req.url))
   }
   return NextResponse.next()
